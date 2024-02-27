35152289afdc5b641510bc6accf221221aeb6fd6
CryoCode <owenramaekers2006@gmail.com>
2024-02-27 15:04:12 +0100

    new file:   css/style.css 	new file:   images/logo.png 	new file:   index.php 	new file:   scripts/script.js

---
+++ index.php
@@ -0,0 +1,26 @@
+<html lang="nl">
+<!--
+
+Name: Owen Ramaekers
+Date: 20/2/2024
+
+-->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trainstation073.nl</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="scripts/script.js"></script>
+</head>
+<body>
+    <div>
+        <ul>
+            <li><a href="index.php"> <img src="images/logo.png" alt="Trainstation073.nl logo"></a></li>
+            <li><a class="menu" href="overons.html">Over ons</a></li>
+            <li><a class="menu" href="contact.html">Contact</a></li>
+            <li id="time"><?php echo date('g:i A'); ?></li>
+        </ul>
+    </div>
+    
+</body>
+</html>
