9925f7c6a829e4edc6f41e82470eda67701d028c
CryoCode <owenramaekers2006@gmail.com>
2024-03-10 19:26:55 +0100

    new file:   css/overons.css 	new file:   images/over.jpg 	modified:   pages/overons.php

--- pages/overons.php
+++ pages/overons.php
@@ -3,8 +3,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Over ons</title>
+    <link rel="stylesheet" href="../css/overons.css">
 </head>
 <body>
     <?php include ('../pages/Topnav.php'); ?>
+    <header id="header">
+    <img src="../images/over.jpg" alt="">
+    <h6>TRAINSTATION 073</h6>
+    <h3>OVER ONZE SPORTSCHOOL</h3>
+    </header>
 </body>
 </html>
