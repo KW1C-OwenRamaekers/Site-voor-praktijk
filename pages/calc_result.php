ccff76ff425e47af5bf9faa5e54629a2a2fafa0e
CryoCode <owenramaekers2006@gmail.com>
2024-03-10 20:32:11 +0100

    new file:   pages/calc_result.php 	modified:   pages/calcalc.php

---
+++ pages/calc_result.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Uitkomst</title>
+</head>
+<body>
+    <h1>Uitkomst</h1>
+    <?php
+    if (isset($_POST['gender'], $_POST['age'], $_POST['frequency'])) {
+        $gender = $_POST['gender'];
+        $age = (int)$_POST['age'];
+        $frequency = (int)$_POST['frequency'];
+        $result = '';}
+
+        if ($gender === 'male') {
+            if ($age <= 16 && $age >= 0) {
+                $result = 'U bent te jong voor de sportschool.';
+            } else {
+            if ($age >= 16 && $age <= 39) {
+                if ($frequency === 0) {
+                    $result = 'U zou minimaal 3 keer per week moeten trainen.';
+                } elseif ($frequency === 1) {
+                    $result = 'U heeft te weinig Trainingen.';
+                }
+            } elseif ($age >= 40 && $age <= 59) {
+                if ($frequency === 0) {
+                    $result = 'U zou minimaal 2 keer per week moeten trainen.';
+                } elseif ($frequency === 1) {
+                    $result = 'U heeft goed gefit.';
+                } elseif ($frequency === 2) {
+                    $result = 'U heeft een te hoge trainingspoor.';
+                }
+            } elseif ($age >= 60) {
+                if ($frequency === 0) {
+                    $result = 'U moet minimaal 1 keer per week trainen.';
+                } elseif ($frequency === 1) {
+                    $result = 'U heeft goed gefit.';
+                }
+            }
+        } if ($gender === 'female') {
+            if ($age <= 16 && $age >= 0) {
+                $result = 'U bent te jong voor de sportschool.';
+            } else {
+            }
+            if ($age >= 16 && $age <= 39) {
+                if ($frequency === 0) {
+                    $result = 'U moet minimaal 2 keer per week trainen.';
+                } elseif ($frequency === 1) {
+                    $result = 'U heeft een te laag trainingspoor.';
+                } elseif ($frequency === 2) {
+                    $result = 'U heeft goed gefit.';
+                } elseif ($frequency === 3) {
+                    $result = 'U heeft te veel Trainingen.';
+                }
+            } elseif ($age >= 40 && $age <= 59) {
+                if ($frequency === 0) {
+                    $result = 'U moet minimaal 1 keer per week trainen.';
+                } elseif ($frequency === 1) {
+                    $result = 'U heeft goed gefit.';
+                }
+            } elseif ($age >= 60) {
+                if ($frequency === 0) {
+                    $result = 'U moet minimaal 1 keer per week trainen.';
+                } elseif ($frequency === 1) {
+                    $result = 'U heeft goed gefit.';
+                }
+            }
+        }
+
+        echo "<p>$result</p>";
+    } else {
+        echo "<p>U heeft niet alles ingevuld.</p>";
+    }
+    ?>
+</body>
+</html>
--- pages/calcalc.php
+++ pages/calcalc.php
@@ -5,12 +5,34 @@
     <title>Calculator</title>
     <link rel="stylesheet" href="../css/calc.css">
     <link rel="stylesheet" href="../css/topnav.css">
+    <script>
+        function popup(url) {
+          window.open(url, 'popup', 'width=600,height=600');
+        }
+    </script>
 </head>
 <body>
     <?php include ('Topnav.php'); ?>
     <article id="calc">
     <h1>Calorie Calculator</h1>
-    
+    <form action="calc_result.php" method="post" target="popup" onsubmit="popup(this.getAttribute('action'))">
+    <p>Geslacht:
+        <input type="radio" name="gender" value="male"> man
+        <input type="radio" name="gender" value="female"> vrouw
+    </p>
+    <p>Leeftijd: <input type="number" name="age" min="10" max="100"></p>
+    <p>Hoe vaak per week traint u? <select name="frequency">
+        <option value="0">niet
+        <option value="1">1 keer
+        <option value="2">2 keer
+        <option value="3">3 keer
+        <option value="4">4 keer
+        <option value="5">5 keer
+        <option value="6">6 keer
+        <option value="7">elke dag
+    </select></p>
+    <p><input type="submit" value="Bereken"></p>
+    </form>
     </article>
     </body>
 </html>
